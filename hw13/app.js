524820bda8e9bcfaed0cd91d97c7b9c6b7f9eac6
VladStroi <vladislavstroy@gmail.com>
2023-01-28 16:52:03 +0200

    new version

--- hw13/app.js
+++ hw13/app.js
@@ -1,17 +1,22 @@
-fetch('https://jsonplaceholder.typicode.com/users')
-.then((response) => {
-  return response.json();
-})
-.then((data) => {
-    const arr = []
-    data.forEach((element, i) => {
-      let obj = {}
-      obj.firstName = data[i].name.split(' ')[0]
-      obj.lastName = data[i].name.split(' ')[1];
-      arr.push(obj)
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    const arr = [];
+    data.map((element) => {
+
+      const splited = element.name.split(" ");
+
+      if (splited[0] == "Mrs." || splited[0] == "Mr.") {
+        arr.push({ firstName: splited[1], lastName: splited[2] });
+      } else {
+        arr.push({ firstName: splited[0], lastName: splited[1] });
+      }
+      
     });
-    return arr
-})
-.then((array) => {
-  console.log(array);
-})
+    return arr;
+  })
+  .then((array) => {
+    console.log(array);
+  });
