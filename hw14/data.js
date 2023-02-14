3c80a46c524b7572483272af8cb36d5fc4a3ad6c
VladStroi <vladislavstroy@gmail.com>
2023-02-14 15:49:53 +0200

    edit task

--- hw14/data.js
+++ hw14/data.js
@@ -1,5 +1,5 @@
-function isBeginOfMonth() {
-    const date = new Date();
+// const date = new Date(2022, 1, 24, 4, 30);
+function isBeginOfMonth(date ) {
     return date.getDate() == 1;
   }
   module.exports = {isBeginOfMonth}
--- hw14/data.test.js
+++ hw14/data.test.js
@@ -1,6 +1,8 @@
 const testExemp = require('./data');
 
 test('if first day of month > (is true), if not > (is false', () => {
-    expect(testExemp.isBeginOfMonth()).toBe(false) //перевірка на коректність виконання тесту
-    expect(testExemp.isBeginOfMonth()).toBe(true) //перевірка умови задачі
+    expect(testExemp.isBeginOfMonth(new Date(2022, 1, 24, 4, 30))).toBe(false) //перевірка на коректність виконання тесту
+});
+test('if first day of month > (is true), if not > (is false', () => {
+    expect(testExemp.isBeginOfMonth(new Date(2022, 1, 24))).toBe(true)  //перевірка умови задачі
 });
